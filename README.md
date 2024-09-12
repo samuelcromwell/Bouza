10756051e20ebd45c9ebfa3cc0c311a7dad7c32e
samuelcromwell <cromwellsamuel3@gmail.com>
2024-09-13 01:51:06 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a repo for Bouza Logistics Company website
---
+++ out/_next/static/chunks/app/ocean-freight/page-4b565a36b1b7c81a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[81],{},function(n){n.O(0,[631,492,422,629,971,23,744],function(){return n(n.s=6629)}),_N_E=n.O()}]);
